13aac11365158c58dd8b880cee2c173f0602d844
tanishq1g <tanishq.gupta@iiitb.org>
2018-01-12 23:09:46 +0530

    chat schema added

--- models/chat.js
+++ models/chat.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 
 var chatSchema = new mongoose.Schema({
-
+    user_name : String,
+    user_comment: String,
 }, {usePushEach: true});
 
 module.exports = mongoose.model('Chat', chatSchema);
