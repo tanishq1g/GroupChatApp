d7876025208237fd38ba7aa8636c97d44d68a17c
tanishq1g <tanishq.gupta@iiitb.org>
2018-01-16 15:31:14 +0530

    post route working

--- public/js/chats/index.js
+++ public/js/chats/index.js
@@ -1,5 +1,32 @@
 var chat_id = -1;
 
+
+function add_comment(){
+    alert("Ds");
+    if( $('#username').val().length == 0 ) {
+		alert('Please Enter a User Name');
+		// return false;
+	}
+    $.ajax({
+        url: "/chats",
+        type: 'POST',
+        data : {
+            "chat_id" : chat_id,
+            "user_name": $('#username').val(),
+            "user_comment": $('#add_comment').val(),
+        },
+        // data :chat_id,
+        dataType: 'json',
+        success: function(response){
+            console.log(response);
+        },
+        error: function(error){
+            console.log("some error occured in post route"+error);
+        }
+    });
+    // return false;
+}
+
 function update_chat(){
     $.ajax({
         url : "/chats/" + chat_id,
@@ -8,12 +35,11 @@ function update_chat(){
         success: function(response){
             console.log(response);
             $('#chats').val( $('#chats').val() + response.chats[0].user_name + " : " + response.chats[0].user_comment + '\n');
-
         },
         error: function(error){
-            console.log("some error occured"+error);
+            console.log("some error occured in get route" + error);
         }
     });
 }
 
-setInterval(update_chat,2000)
+setInterval(update_chat,8000);
--- routes/chats.js
+++ routes/chats.js
@@ -5,6 +5,7 @@ var Chat = require('../models/chat');
 
 //Root route
 router.get("/:chat_id",function(req,res){
+    console.log('get route reached');
     Chat.find({id : {$gt : req.params.chat_id}}, function(err,foundchats){
         if(err){
             console.log("some error in finding the correct chats");
@@ -16,5 +17,10 @@ router.get("/:chat_id",function(req,res){
 });
 
 
-
+router.post('/',function(req,res){
+    console.log('post route reached');
+    var data = req.body;
+    console.log(req);
+    res.send(data);
+});
 module.exports = router;
